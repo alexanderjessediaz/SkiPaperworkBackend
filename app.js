8823a5a05459669a392fbcf036bfc78509cafa72
AJ Diaz <ajdiaz@trouthousetech.com>
2023-04-11 16:34:46 -0600

    remove old code

--- app.js
+++ app.js
@@ -5,24 +5,12 @@ const app = express();
 const corsPort = 8000
 
 const PORT = process.env.PORT || 5555;
-app.use(express.static('public'));
-app.use(express.json());
 app.use(cors());
         
-app.get('/', (req, res) => {
-  res.status(200).json({info: 'preset text'})
-})
 
-app.post('/', (req, res) =>  {
-  const { parcel } = req.body
-  console.log(parcel)
-  if (!parcel) {
-    return res.status(400).send({ status: 'failed'})
-  }
-  res.status(200).send({ status: 'recieved'})
-})
-
-app.listen(PORT, () => console.log(`app has started on port: ${PORT}`))
-app.listen(corsPort, function () {
-  console.log(`CORS-enabled web server listening on ${corsPort}`)
+app.get("/products/:id", function (req, res, next) {
+  res.json({msg: "This is CORS-enabled for all origins!"})
+});
+app.listen(5555, function () {
+  console.log(`CORS-enabled web server listening on ${PORT}`)
 });
